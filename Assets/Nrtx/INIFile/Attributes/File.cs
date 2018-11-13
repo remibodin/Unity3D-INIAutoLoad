e2f926fae3d43b3602d4710636b464661ee0bcf4
Remi Bodin <remi.bodin@gmail.com>
2018-11-13 08:32:48 +0100

    Add comment for public attributes

--- Assets/Nrtx/INIFile/Attributes/File.cs
+++ Assets/Nrtx/INIFile/Attributes/File.cs
@@ -3,6 +3,10 @@ namespace IniConfiguration.Attributes
     [System.AttributeUsage(System.AttributeTargets.Class)]
     public class File : System.Attribute
     {
+        /// <summary>
+        /// Attribute use to define the path of the file use to override values
+        /// </summary>
+        /// <param name="filepath">The filepath relative to the executable path</param>
         public File(string filepath)
         {
             this.filepath = filepath;
--- Assets/Nrtx/INIFile/Attributes/Section.cs
+++ Assets/Nrtx/INIFile/Attributes/Section.cs
@@ -2,6 +2,10 @@ namespace IniConfiguration.Attributes
 {
     public class Section : System.Attribute
     {
+        /// <summary>
+        /// Attribute use to make a public static variable overrided
+        /// </summary>
+        /// <param name="name">The section name in ini file</param>
         public Section(string name)
         {
             this.name = name;
